59e3bd0bcdb26001b158c5fe90e71e0ef98be8ac
RobinTang <sintrb@gmail.com>
2014-08-28 22:01:55 +0800

    Add Demo Project & Sin.Http.Request Demo

---
+++ Demo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using System.Diagnostics;
+
+using Demo.Resources;
+
+namespace Demo
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+
+            foreach (Button b in (from n in spMain.Children where n is Button select n))
+            {
+                b.Click += b_Click;
+            }
+        }
+
+        void b_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine((sender as Button).Tag as String);
+            NavigationService.Navigate(new Uri((sender as Button).Tag as String, UriKind.Relative));
+        }
+    }
+}
---
+++ Demo/RequestPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Sin.Http;
+
+namespace Demo
+{
+    public partial class RequestPage : PhoneApplicationPage
+    {
+        Request req = new Request();
+        public RequestPage()
+        {
+            InitializeComponent();
+        }
+        private void goRequest(object sender, RoutedEventArgs e)
+        {
+            req.GET(txtUrl.Text, (ok, co) =>
+            {
+                String res = co.ResponseText;
+                Dispatcher.BeginInvoke(() =>
+                {
+                    tbResponse.Text = res;
+                });
+                return ok;
+            });
+        }
+    }
+}
