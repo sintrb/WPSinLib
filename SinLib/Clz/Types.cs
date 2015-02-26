08ee5c20d744238f9a2cd8f607cb18ecd728650e
RobinTang <sintrb@gmail.com>
2015-02-26 18:04:51 +0800

    Clz

---
+++ SinLib/Clz/Types.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sin.Clz.Types
+{
+    public class SelectorArray
+    {
+        public List<Object> _Values = null;
+        public List<Object> Values
+        {
+            get
+            {
+                if (_Values == null)
+                    _Values = new List<object>();
+                return _Values;
+            }
+            set
+            {
+                _Values = value;
+            }
+        }
+    }
+}
---
+++ SinLib/Clz/ValueConverters.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Sin.Clz.Conv
+{
+    public class BoolToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value == Visibility.Visible);
+        } 
+    }
+
+    public class BoolToNotVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(bool)value)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value != Visibility.Visible);
+        }
+    }
+
+    public class BoolToHorizontalAlignment : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return HorizontalAlignment.Left;
+            else
+                return HorizontalAlignment.Right;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value != Visibility.Visible);
+        }
+    }
+
+    public class VisibleWhenNotEmpty : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value==null || (value is bool && !(bool)value) || (value is String && String.IsNullOrEmpty((String)value)))
+                return Visibility.Collapsed;
+            else
+                return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Visibility)value == Visibility.Visible);
+        }
+    }
+
+    public class WithDefaultValue : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return parameter;
+            return (value == null || value is String) && String.IsNullOrEmpty((String)value) ? (parameter == null ? "null" : parameter) : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class IndexSelector : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int ix = 0;
+            if (value is bool && (bool)value)
+                ix = 1;
+            else if (value is int)
+                ix = (int)value;
+
+            if (parameter is Types.SelectorArray)
+            {
+                Types.SelectorArray arr = parameter as Types.SelectorArray;
+                if (arr.Values != null && arr.Values.Count!=0)
+                {
+                    return ix < arr.Values.Count ? arr.Values[ix] : arr.Values[0];
+                }
+            }
+            System.Diagnostics.Debug.WriteLine("error: " + value + " in " + parameter);
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
